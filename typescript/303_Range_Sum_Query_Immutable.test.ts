4d35b1a539701d42025eaefad6346eef4cdcf2c8
narawit-tub <narawit.tub@outlook.co.th>
2023-05-06 16:33:01 +0700

    optimize 303, use prefix sum technique

--- typescript/303_Range_Sum_Query_Immutable.test.ts
+++ typescript/303_Range_Sum_Query_Immutable.test.ts
@@ -2,15 +2,18 @@ import { test, expect } from "vitest";
 
 class NumArray {
   nums: number[];
+  prefixSum: number[];
   constructor(nums: number[]) {
-    this.nums = nums;
+    // Create prefixSum arrays
+    this.prefixSum = new Array<number>(nums.length + 1).fill(0);
+
+    for (let i = 1; i < this.prefixSum.length; i++) {
+      this.prefixSum[i] = this.prefixSum[i - 1] + nums[i - 1];
+    }
   }
 
   sumRange(left: number, right: number): number {
-    let sum = 0;
-    for (let i = left; i <= right; i++) {
-      sum += this.nums[i];
-    }
+    const sum = this.prefixSum[right + 1] - this.prefixSum[left];
     return sum;
   }
 }
