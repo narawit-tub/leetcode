89a1c7eb8fe0738efe8209dfaefd2e9760d08c9b
narawit-tub <narawit.tub@outlook.co.th>
2023-06-05 22:35:06 +0700

    560 optimize using prefixSum, by looking for the subArray that can subtract to getk

--- typescript/560_subarray_sum_equals_k.test.ts
+++ typescript/560_subarray_sum_equals_k.test.ts
@@ -21,8 +21,32 @@ function solutionOne(nums: number[], k: number): number {
   return numberOfSubarrayCount;
 }
 
+// if the result of accumulatedSum - k found in sumOfSubArraySoFarFromStart, it's mean we are able to use that subArray to subtract the accumulatedSum
+// and got new subArray which is equal to 'k'
+// accumulatedSum - k = sumOfSubArraySoFarFromStart[i] so...
+// accumulatedSum - sumOfSubArraySoFarFromStart[i] = k <----- HERE IT IS !!! -->
+function solutionTwo(nums: number[], k: number): number {
+  let accumulatedSum = 0;
+  let count = 0;
+  let sumOfSubArraySoFarFromStart = {
+    0: 1,
+  };
+
+  for (let index = 0; index < nums.length; index++) {
+    accumulatedSum += nums[index];
+    if (sumOfSubArraySoFarFromStart[accumulatedSum - k]) {
+      count = count + sumOfSubArraySoFarFromStart[accumulatedSum - k];
+    }
+
+    if (sumOfSubArraySoFarFromStart[accumulatedSum]) {
+      sumOfSubArraySoFarFromStart[accumulatedSum]++;
+    } else sumOfSubArraySoFarFromStart[accumulatedSum] = 1;
+  }
+  return count;
+}
+
 function subArraySumEqualK(nums: number[], k: number): number {
-  return solutionOne(nums, k);
+  return solutionTwo(nums, k);
 }
 
 test.each([
@@ -31,6 +55,7 @@ test.each([
   { nums: [1, 2, 3, 4], k: 3, expected: 2 },
   { nums: [3, 4, 7, 2, -3, 1, 4, 2], k: 7, expected: 4 },
   { nums: [0, 0, 0, 0, 0, 0, 0, 0], k: 0, expected: 36 },
+  { nums: [2, 3, 3, -1, 0, 7, -2, 5], k: 5, expected: 6 },
   { nums: [-1, -1, 1], k: 0, expected: 1 },
 ])("Expect $expected", ({ nums, k, expected }) => {
   expect(subArraySumEqualK(nums, k)).toBe(expected);
