462dfc6f3a13ace7ed5c4b293567c7ce5502719b
narawit-tub <narawit.tub@outlook.co.th>
2023-08-10 00:31:36 +0700

    235 optimized

--- golang/235_lowest_common_ancentor_of_binary_search_tree/235_lowest_common_ancestor_of_binary_search_tree.go
+++ golang/235_lowest_common_ancentor_of_binary_search_tree/235_lowest_common_ancestor_of_binary_search_tree.go
@@ -33,3 +33,20 @@
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
     return find(root, p, q)
 }
+
+// another optimized version
+
+// func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+//     for root != nil {
+//             // if root more than both p, q it's means there can be new root which value less then current one, so let's ship root node to root.left
+//         if (root.Val > p.Val && root.Val > q.Val) {
+//             root = root.Left
+//         } else if (root.Val < p.Val && root.Val < q.Val) {
+//             // if root less than both p, q, it's mean there can be new root which its value more than current root, so let's ship root node to root.Right
+//             root = root.Right
+//         } else {
+//             return root
+//         }
+//     }
+//     return nil
+// }
